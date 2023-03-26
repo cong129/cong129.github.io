8c1665084d8f83fdd427d587435c01ed83d64378
cong129 <83438650+cong129@users.noreply.github.com>
2023-03-26 22:06:33 +0900

    Update script.js

--- resource/script.js
+++ resource/script.js
@@ -1,9 +1,14 @@
-//DOM
-titleSlide = document.querySelector(".titleSlide");
-slides = document.querySelectorAll(".slide");
+// SLIDEバージョンとアニメーションバージョンを用意している。
+// 11~52がSLIDEバージョン,55~106がアニメーションバージョン。
 
+//DOM
+const titleSlide = document.querySelector(".titleSlide");
+const slides = document.querySelectorAll(".slide");
+const hiddenElements = document.querySelectorAll(".hidden");
 //Variable define
 let slideNumber = 0;
+
+//-------------------- SLIDE version -------------------------------
 
 //Defualt Setting
 for (const slide of slides){
@@ -13,7 +18,6 @@ for (const slide of slides){
 //Functions
 function nextSlide(){
     slides[slideNumber].style.display = "none";
-
     if(slideNumber+1 < slides.length){
         slides[slideNumber+1].style.display = "grid";
     }
@@ -44,3 +48,59 @@ for (let i = 0 ; i < slides.length ; i++){
     // console.log(slides[i]);
     slides[i].addEventListener("click",nextSlide);
 }
+
+//---------------------------SLIDE version----------------------------
+
+
+//---------------------------SCROLL version---------------------------
+
+// document.body.style.backgroundImage="url('picture/transparent.jpg')";
+// document.body.style.color="#000";
+
+// //Defualt Setting
+// for (const slide of slides){
+//     slide.classList.add("hidden");
+// }
+
+// //Functions
+// function nextSlide(){
+    
+//     slides[slideNumber].classList.add("hidden");
+//     slides[slideNumber].classList.remove("show");
+    
+//     if(slideNumber+1 < slides.length){
+//         slides[slideNumber+1].classList.remove("hidden");
+//         slides[slideNumber+1].classList.add("show");
+//     }
+//     slideNumber++;
+//     console.log(slideNumber);
+    
+//     if(slideNumber===slides.length){
+//         restartPresent();
+//     }
+// }
+
+// function presentStart(){
+//     titleSlide.classList.add("hidden");
+//     titleSlide.classList.remove("show");
+//     slides[slideNumber].classList.add("show");
+//     slides[slideNumber].classList.remove("hidden");
+// }
+
+// function restartPresent(){
+//     slideNumber=0;
+//     for (let i = 0 ; i < slides.length ; i++){
+//         slides[i].classList.add("hidden");
+//         slides[i].classList.remove("show");
+//     }    
+//     titleSlide.classList.add("show");
+// }
+
+// //Event
+// titleSlide.addEventListener("click",presentStart);
+// for (let i = 0 ; i < slides.length ; i++){
+//     // console.log(slides[i]);
+//     slides[i].addEventListener("click",nextSlide);
+// }
+
+//---------------------------SCROLL version---------------------------
